1a2ce5e1eb14eee01516a87ac8b50a353673125d
kumzz <hi.kumz@gmail.com>
2017-01-03 18:46:31 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/serviceworker/sw.js', { scope: '/serviceworker/' }).then(function(reg) {
+    
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+    
+  }).catch(function(error) {
+    console.log('Registration failed with ' + error);
+  });
+  
+  navigator.serviceWorker.onmessage = function (event) {
+    var message = JSON.parse(event.data);
+    
+    var isRefresh = message.type === 'refresh';
+    if (isRefresh) {
+      console.log('Received a message from service worker');
+    }
+    var isAsset = message.url.includes('asset');
+    console.log('Reloading page');
+    location.reload();
+  }
+}
+
